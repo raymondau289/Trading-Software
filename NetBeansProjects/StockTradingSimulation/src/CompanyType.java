2cd1456672b2a410981e9f234129bd6a13cbdfd0
nsb24 <nsb24@sussex.ac.uk>
2017-05-03 21:48:43 +0100

    Update CompanyType.java

--- NetBeansProjects/StockTradingSimulation/src/CompanyType.java
+++ NetBeansProjects/StockTradingSimulation/src/CompanyType.java
@@ -2,7 +2,7 @@
  * this enum describes the categories which company objects belong to.
  * 
  * @author Navtej bhogal
- * @version 1.0 03 may 2017
+ * @version 1.0, 03 may 2017
  */
 public enum CompanyType 
 {
