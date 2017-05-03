a783ef8ebb8518105a3015a21a51a945de1211fd
nsb24 <nsb24@sussex.ac.uk>
2017-05-03 21:46:56 +0100

    Update CompanyType.java

--- NetBeansProjects/StockTradingSimulation/src/CompanyType.java
+++ NetBeansProjects/StockTradingSimulation/src/CompanyType.java
@@ -1,4 +1,9 @@
-// this enum describes the categories which company objects belong to.
+/**
+ * this enum describes the categories which company objects belong to.
+ * 
+ * @author Navtej bhogal
+ * @version 1.0 03 may 2017
+ */
 public enum CompanyType 
 {
     HITECH,FOOD,HARD,PROPERTY
